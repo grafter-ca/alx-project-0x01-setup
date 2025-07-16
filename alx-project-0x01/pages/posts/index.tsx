5a35a0c14fb7522d37e1faedcfc40b70e1e70c99
grafter-ca <caleb.designer1@gmail.com>
2025-07-17 00:30:53 +0200

    implement fetch API

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,27 +1,39 @@
-import React from "react";
-import PostCard from "../../components/common/PostCard";
+import PostCard from "@/components/common/PostCard";
 import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 
-const dummyPosts = [
-  { id: 1, title: "Post One", body: "This is the first post." },
-  { id: 2, title: "Post Two", body: "This is the second post." },
-];
-
-const PostsPage: React.FC = () => {
+const Posts: React.FC<{ posts: PostProps[] }> = ({posts}) => {
+  console.log(posts)
   return (
+    <div className="flex flex-col h-screen">
+      <Header />
+      <main className="p-4">
+        <div className="flex justify-between">
+        <h1 className=" text-2xl font-semibold">Post Content</h1>
+        <button className="bg-blue-700 px-4 py-2 rounded-full text-white">Add Post</button>
+        </div>
+        <div className="grid grid-cols-3 gap-2 ">
+          {
+            posts?.map(({ title, body, userId, id }: PostProps, key: number) => (
+              <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+            ))
+          }
+        </div>
+      </main>
+    </div>
+  )
+}
 
-    <>
-    <Header />
-    <main className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Posts</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        {dummyPosts.map((post) => (
-          <PostCard key={post.id} title={post.title} body={post.body} />
-        ))}
-      </div>
-    </main>
-    </>
-  );
-};
 
-export default PostsPage;
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts")
+  const posts = await response.json()
+
+  return {
+    props: {
+      posts
+    }
+  }
+}
+
+export default Posts;
