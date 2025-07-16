dfb6aad5e580ed5639a5709ca67cdc60e4ab3c1c
grafter-ca <caleb.designer1@gmail.com>
2025-07-17 00:47:11 +0200

    implement user card

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,32 +1,34 @@
-import Header from "@/components/layout/Header";
 import React from "react";
+import Header from "@/components/layout/Header";
+import UserCard from "@/components/common/UserCard";
+import { UserProps } from "@/interfaces";
 
-const dummyUsers = [
-  { id: 1, name: "Alice Johnson", email: "alice@example.com" },
-  { id: 2, name: "Bob Smith", email: "bob@example.com" },
-];
 
-const UsersPage: React.FC = () => {
+const UsersPage: React.FC<{users:UserProps[]}> = ({ users }) => {
   return (
-
     <>
       <Header />
-    <main className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Users</h1>
-      <ul className="space-y-4">
-        {dummyUsers.map((user) => (
-          <li
-            key={user.id}
-            className="p-4 bg-white shadow rounded hover:shadow-md transition"
-          >
-            <h2 className="text-xl font-semibold">{user.name}</h2>
-            <p className="text-gray-700">{user.email}</p>
-          </li>
-        ))}
-      </ul>
-    </main>
+      <main className="p-6">
+        <h1 className="text-3xl font-bold mb-6 text-center">Users</h1>
+        <div className="grid grid-cols-6 gap-6">
+          {users.map((user) => (
+            <UserCard key={user.id} {...user} />
+          ))}
+        </div>
+      </main>
     </>
   );
 };
+
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const users = await response.json();
+
+  return {
+    props: {
+      users,
+    },
+  };
+}
 
 export default UsersPage;
