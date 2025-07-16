7094aef9cfacbca4b48a73af8eff77acc541db83
grafter-ca <caleb.designer1@gmail.com>
2025-07-17 00:50:48 +0200

    update name users to posts

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -4,14 +4,14 @@ import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
 
-const UsersPage: React.FC<{users:UserProps[]}> = ({ users }) => {
+const UsersPage: React.FC<{posts:UserProps[]}> = ({ posts }) => {
   return (
     <>
       <Header />
       <main className="p-6">
         <h1 className="text-3xl font-bold mb-6 text-center">Users</h1>
         <div className="grid grid-cols-6 gap-6">
-          {users.map((user) => (
+          {posts.map((user) => (
             <UserCard key={user.id} {...user} />
           ))}
         </div>
@@ -21,14 +21,14 @@ const UsersPage: React.FC<{users:UserProps[]}> = ({ users }) => {
 };
 
 export async function getStaticProps() {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users = await response.json();
+  const response = await fetch("https://jsonplaceholder.typicode.com/users")
+  const posts = await response.json()
 
   return {
     props: {
-      users,
-    },
-  };
+      posts
+    }
+  }
 }
 
 export default UsersPage;
